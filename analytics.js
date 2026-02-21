56dd1241304d72ad55a160a02228aae53243467a
OTFdonny <75620142+OTFdonny@users.noreply.github.com>
2026-02-21 11:44:20 -0500

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,13 +1,10 @@
-// Load Google Analytics properly
-var gtagScript = document.createElement("script");
-gtagScript.async = true;
-gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-6WGWECFCHR";
-document.head.appendChild(gtagScript);
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-
-gtagScript.onload = function() {
+document.write(`
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6WGWECFCHR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'G-6WGWECFCHR');
-};
+</script>
+`);
